b7c0752a9f39fcad42d1309397f6ae0fbc2b8d4f
KingMeTo92 <34463714+dramon4@users.noreply.github.com>
2020-02-17 15:52:18 +0900

    #2.4 Handling Routes with Express
    Get: User가 URL입력 했을시 브라우저에서 Get 메소드를 실행해서 웹사이트 페이지를 읽어온다.
    
    POST: User가 ID/Password등 정보값을 입력하면 브라우저가 POST메소드를 실행하여서 웹사이트에 정보를 전달한다.
    
    (req, res) ->
    req : request Object는 정보를 받는 객체(인자)이다.
    res: response Object는 정보에 응답하는 객체(인자)이다.

--- index.js
+++ index.js
@@ -7,4 +7,15 @@ const handleListening = () => {
   console.log(`Listening On: http://localhost:${PORT}`);
 };
 
+const handleHome = (req, res) => {
+  res.send("Hello My HomPage");
+};
+
+const handleProfile = (req, res) => {
+  res.send("Hi my ProfilePage");
+};
+
+app.get("/", handleHome);
+app.get("/profile", handleProfile);
+
 app.listen(PORT, handleListening);
