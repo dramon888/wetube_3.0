62eb020440a6fea99027f2e05509e3db348023b1
KingMeTo92 <34463714+dramon4@users.noreply.github.com>
2020-02-18 13:13:42 +0900

    #2.5 ES6 on NodeJS using Babel
    @babel을 설치했다.
    바벨이란?.
    최신코드의 문법을 (구)코드로 변환해주어서 최신코드를 지원하게 해준다.
    바벨 설치방법.
    -npm install @babel/node
    -npm install @babel/preset-env
    (.bebelrc)에 preset-env를 사용하겠다고 설정해준다.
    Package.json {"script" : "nodemon --exec babel-node index.js"}
    --nodemon : 서버를 재실행 하지 않고 저장하면 자동으로 재시작 하게 해주는 Package이다.
    -npm install nodemon -D
    -D: 는 package.json에서 프로젝트를 위한 묶음이아니라, 프로젝트-개발자의 entryPoint로 저장 할 수 있다.
    ("devDependencies")

--- index.js
+++ index.js
@@ -1,19 +1,15 @@
-const express = require("express");
+// const express = require("express");
+import express from "express";
 const app = express();
 
 const PORT = 4000;
 
-const handleListening = () => {
+const handleListening = () =>
   console.log(`Listening On: http://localhost:${PORT}`);
-};
 
-const handleHome = (req, res) => {
-  res.send("Hello My HomPage");
-};
+const handleHome = (req, res) => res.send("Hello My HomPage");
 
-const handleProfile = (req, res) => {
-  res.send("Hi my ProfilePage");
-};
+const handleProfile = (req, res) => res.send("Hi my ProfilePage");
 
 app.get("/", handleHome);
 app.get("/profile", handleProfile);
