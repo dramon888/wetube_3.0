d7fe28a9347af6a71afe238a7bec3afd78439f31
KingMeTo92 <34463714+dramon4@users.noreply.github.com>
2020-02-21 15:44:21 +0900

    #2.9 MVC Pattern part One
    MVC patten
    : M model (data)
      V view (how does the data look)
      C control (function that looks for the data)
    
    router 폴더안에  router.js들을 나눈다.
    URL / Function 을
    각각 MVC 패턴에 맞게 나누는 과정이다.

--- app.js
+++ app.js
@@ -4,13 +4,11 @@ import morgan from "morgan";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
 import helmet from "helmet";
-import { userRouter } from "./router";
+import globalRouter from "./router/globalRouter";
+import userRouter from "./router/userRouter";
+import videoRouter from "./router/videoRouter";
 
 const app = express();
-
-const handleHome = (req, res) => res.send("Hello My HomPage");
-
-const handleProfile = (req, res) => res.send("Hi my ProfilePage");
 
 app.use(cookieParser());
 app.use(bodyParser.json());
@@ -18,9 +16,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(helmet());
 app.use(morgan("dev"));
 
-app.get("/", handleHome);
-app.get("/profile", handleProfile);
-
+app.use("/", globalRouter);
 app.use("/user", userRouter);
+app.user("/video", videoRouter);
 
 export default app;
