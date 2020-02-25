f019951f51f9e31a8742272a2964210080d158db
KingMeTo92 <34463714+dramon4@users.noreply.github.com>
2020-02-25 16:05:56 +0900

    #2.25 More Controlers
    1.videoDetail 화면
    mixin 과 home/search등 video를 활용하는 템플릿에서 video를 클릭하면 해당.id값 videoDetail(videos.id)화면으로 출력하게 구현하였다.
    2.log out
    res.redirect(routes.home)으로만 구현하였다. *현재 사용자 인증을 하지 않았다.
    3.upload
    upload 템플릿에서 const {}=req;
    로 file, title, description을 가져오는 것 까지 구현하였고, 실제 DB가 없기 때문에 추후에 저장기능을 구현해야한다. 그래서 임시방편으로 res.redirect(routes.videoDetail(3423423)); videoDetail id를 임의값을 주어서 업로드하고나면 해당아이디 videos상세페이지 화면으로 들어가게 구현하였다.

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,4 +1,5 @@
 import { videos } from "../db";
+import routes from "../routes";
 
 export const home = (req, res) => {
   res.render("home", { pageTitle: "Home", videos });
@@ -17,8 +18,17 @@ export const search = (req, res) => {
     videos: videos
   });
 };
-export const upload = (req, res) =>
+export const getUpload = (req, res) => {
   res.render("upload", { pageTitle: "upload" });
+};
+export const postUpload = (req, res) => {
+  const {
+    body: { file, title, description }
+  } = req;
+  //To Do: Upload and save video
+  res.redirect(routes.videoDetail(324393));
+};
+
 export const videoDetail = (req, res) =>
   res.render("videoDetail", { pageTitle: "videoDetail" });
 export const editVideo = (req, res) =>
