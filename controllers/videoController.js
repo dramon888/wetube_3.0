6216b98602c6654a638be7df0c677f19a8c25609
KingMeTo92 <34463714+dramon4@users.noreply.github.com>
2020-03-04 13:49:16 +0900

    #3.8 Getting Video by ID
    1. videoDetail을 누르면 video의 ID를 받고 Video의 자세한 정보를 생성하는 페이지를 구현해보자.
    
    2. Search에 Query로 term(단어)를 받은것처럼, video ID를 받을 것 이다.
    
    3. Controller에서 어떤 data를 가지고 있다는 것을 표현하고 싶다면 :id, :potato, :감자 이렇게 이름을 표기해서 req.params를 하면 URL에서 해당하는 값을(params) 가져올 수 있다.
    
    4. 가져온 ID 값을
    await Video.findByID(id);
    findByID로 parameter값을 좀 전에 찾은 id값을 주고 Video.js에서 해당하는 (findByID(id)이니까) id값을 찾아서 정보를 가져오고, render({video})로 View-videoDetail로 video의 정보들을 전달해서 랜더링 해준다.

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -43,8 +43,20 @@ export const postUpload = async (req, res) => {
   res.redirect(routes.videoDetail(newVideo.id));
 };
 
-export const videoDetail = (req, res) =>
-  res.render("videoDetail", { pageTitle: "videoDetail" });
+export const videoDetail = async (req, res) => {
+  const {
+    params: { id }
+  } = req;
+  try {
+    const video = await Video.findById(id);
+    res.render("videoDetail", { pageTitle: "videoDetail", video });
+    console.log(video);
+  } catch (error) {
+    console.log(error);
+    res.render(routes.home);
+  }
+};
+
 export const editVideo = (req, res) =>
   res.render("editVideo", { pageTitle: "editVideo" });
 export const deleteVideo = (req, res) =>
