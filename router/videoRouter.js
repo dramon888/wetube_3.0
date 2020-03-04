65a229dc0461c3e62f37557adeba37e5146caa62
KingMeTo92 <34463714+dramon4@users.noreply.github.com>
2020-03-04 18:19:41 +0900

    #3.9 Editing a Video
    1. editVideo: 비디오를 수정할 떄 내용은 바꿀수 있되, video는 바꿀 수 없다. 혹시 수정하려면 새로운 video를 생성한다.
    
    2. routes.js에서 editVideo를 /:id 를 함수로 변환해서 HTML도 실제 /:id를 받을 수 있게 function() 으로 바꿔준다.
    
    3. routes에서 videoDetail을 함수로 바꿔줬으면, router에서도 (routes.editVideo())로 바꿔준다.
    
    4. 인제 router / 즉 html의 url이 /:id에 반응을하여 들어가면 controller에서 제어를 해야하는데 videoDetail Controller에서 get과 post로 받는다.
    get으로는 id값을 가져와서 어떤 비디오를 선택할지/ 그비디오라면 제목/설명을 현재값에 넣어준다. 그리고나서 post로 실질적인 업데이트/redirect를 하는데, Video.findOneAndUpdate()메서드를 사용하여 현재값인지 변환값이지 판단해서 저장하게 된다.

--- router/videoRouter.js
+++ router/videoRouter.js
@@ -2,20 +2,24 @@ import express from "express";
 import routes from "../routes";
 import {
   videoDetail,
-  editVideo,
   deleteVideo,
   postUpload,
-  getUpload
+  getUpload,
+  getEditVideo,
+  postEditVideo
 } from "../controllers/videoController";
 import { uploadVideo } from "../localsMiddleware";
 
 const videoRouter = express.Router();
-
+//Add
 videoRouter.get(routes.upload, getUpload);
 videoRouter.post(routes.upload, uploadVideo, postUpload);
-
+//video Detail
 videoRouter.get(routes.videoDetail(), videoDetail);
-videoRouter.get(routes.editVideo, editVideo);
+//Update
+videoRouter.get(routes.editVideo(), getEditVideo);
+videoRouter.post(routes.editVideo(), postEditVideo);
+//Delete
 videoRouter.get(routes.deleteVideo, deleteVideo);
 
 export default videoRouter;
