3e577251847b50aacfaa1aa16e67de50269c8d43
KingMeTo92 <34463714+dramon4@users.noreply.github.com>
2020-03-24 14:18:06 +0900

    #7.0 User Profile
    . Profile 페이지를 수정하였다.
    
    **코드 참고바람**

--- controllers/userController.js
+++ controllers/userController.js
@@ -119,7 +119,7 @@ export const userDetail = async (req, res) => {
     res.redirect(routes.home);
   }
 };
-export const editProfile = (req, res) =>
+export const getEditProfile = (req, res) =>
   res.render("editProfile", { pageTitle: "EditProfile" });
 export const changePassword = (req, res) =>
   res.render("changePassword", { pageTitle: "ChangePassword" });
